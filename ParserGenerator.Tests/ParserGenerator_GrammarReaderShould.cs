f2219fa75a4efe7a4fe1a2931cc3609390440e6d
Murray Coueslant <murray.coueslant@gmail.com>
2020-05-27 20:01:20 +0100

    parser generator writing time.
    first step is to write a PEG parser, which will be a packrat parser.

---
+++ ParserGenerator.Tests/ParserGenerator_GrammarReaderShould.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace ParserGenerator.Tests {
+    public class ParserGenerator_GrammarReaderShould {
+        private ParserGenerator _parserGenerator;
+
+        [SetUp]
+        public void Setup () {
+            _parserGenerator = new ParserGenerator ();
+        }
+
+        [Test]
+        public void ReadCorrectNumberOfNonTerminals () {
+            Assert.Fail ();
+        }
+    }
+}
---
+++ ParserGenerator/ParserGenerator.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace ParserGenerator {
+    public class ParserGenerator { }
+}
